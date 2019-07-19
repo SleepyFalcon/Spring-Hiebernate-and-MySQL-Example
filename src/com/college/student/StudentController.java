ab579df4693ec186840c056adfb41c0df45bc8d0
kyle <kylerooney24@gmail.com>
2019-07-19 10:36:53 -0500

    change
    remove and use some imports

--- src/com/college/student/StudentController.java
+++ src/com/college/student/StudentController.java
@@ -6,9 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
